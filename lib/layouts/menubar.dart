0331f2ce87ca698ce7dd9441aab40abff83a98da
web-liuyang <984584014@qq.com>
2025-07-24 11:14:52 +0800

    Update: Menubar

--- lib/layouts/menubar.dart
+++ lib/layouts/menubar.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+import 'package:path/path.dart' as p;
+
 import 'package:blueprint_master/editors/editors.dart';
 import 'package:flutter/material.dart';
 
@@ -15,27 +18,67 @@ class _MenubarState extends State<Menubar> {
   Widget build(BuildContext context) {
     // final DrawCubit drawCubit = context.watch<DrawCubit>();
 
+    final recent = [
+      "/Users/liuyang/Desktop/project_test/project1.bm",
+      "/Users/liuyang/Desktop/project_test/project2.bm",
+      "/Users/liuyang/Desktop/project_test/project3.bm",
+    ];
+
     return MenuBar(
-      // style: MenuStyle(alignment: Alignment.centerLeft),
+      style: MenuStyle(
+        backgroundColor: WidgetStateProperty.all(Colors.transparent),
+        shadowColor: WidgetStateProperty.all(Colors.transparent),
+        padding: WidgetStateProperty.all(EdgeInsets.zero),
+      ),
       children: [
         SubmenuButton(
-          onClose: () {},
-          onOpen: () {},
-          onFocusChange: (value) {},
-          onHover: (value) {},
           menuChildren: [
             MenuItemButton(
-              child: Text("About"),
+              child: Text("New"),
+              onPressed: () {},
+            ),
+            SubmenuButton(
+              menuChildren: recent
+                  .map(
+                    (e) => MenuItemButton(
+                      child: Text(e),
+                      onPressed: () {},
+                    ),
+                  )
+                  .toList(growable: false),
+              child: Text("Open Recent"),
+            ),
+            MenuItemButton(
+              child: Text("Exit"),
+              onPressed: () {
+                exit(exitCode);
+              },
+            ),
+          ],
+          child: Center(child: Text("File")),
+        ),
+        SubmenuButton(
+          menuChildren: [
+            MenuItemButton(
+              child: Text("Setting"),
               onPressed: () {
                 showAboutDialog(context: context);
               },
             ),
-            MenuItemButton(child: Text("Feedback"), onPressed: () {}),
           ],
-          child: Container(child: Text("Help")),
+          child: Center(child: Text("Setting")),
         ),
-
-        // SubmenuButton(menuChildren: [MenuItemButton(child: Text("Setting"))], child: MenuItemButton(child: Text("Help"))),
+        SubmenuButton(
+          menuChildren: [
+            MenuItemButton(
+              child: Text("About"),
+              onPressed: () {},
+            ),
+            MenuItemButton(child: Text("Feedback"), onPressed: () {}),
+            MenuItemButton(child: Text("Document"), onPressed: () {}),
+          ],
+          child: Center(child: Text("Help")),
+        ),
       ],
     );
   }
